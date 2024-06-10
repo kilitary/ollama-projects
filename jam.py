cde88cc35474cb5b95e542c9bfdd338d85341a98
kilitary <kilitary@gmail.com>
2024-06-10 21:10:18 +0300

    insertio

--- jam.py
+++ jam.py
@@ -1,15 +1,19 @@
 import winsound
+import random
 
 # Play Windows ext sound.
 # method: rprop
-for x in range(1, 50):
+for x in range(random.randrange(2, 10), 10):
     x += 10
     for a in range(1, x):
         for i in range(1, a):
-            idel = i * 10
+            idel = int(i * 10)
             frq = 1300
-            frq += i * idel
-            print(frq)
-            winsound.Beep(frq, 128 + idel + a + x)
+            frq += int(i * idel)
+            print(f'{frq} {idel} {x} {a}')
+            winsound.Beep(frq, int(128 + idel + a + x))
+        if x >= 16:
+            print('uniform strip')
+            x = int(random.uniform(2, 50))
 
 print('terminated')
