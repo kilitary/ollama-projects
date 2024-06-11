200771726ba494f9e98a20173ff8071a388efe9e
kilitary <kilitary@gmail.com>
2024-06-11 13:04:10 +0300

    insertio

--- jam.py
+++ jam.py
@@ -2,7 +2,7 @@
 # date: 2020-03-01
 # so liar
 
-# date: later
+# d at e: la ter
 # secret: se c ret
 # for: quiery
 # type: help
@@ -19,9 +19,10 @@ idel = 0
 d = 0
 for y in range(1, 9):
     print(f'floOr {y}')
-    for x in range(random.randrange(5, 10), 30):
-        x += 10
-        for a in range(1, x - d):
+    for x in range(random.randrange(6, 8), 35):
+        x += 3
+        for a in range(3, x - d):
+            a = random.randrange(1, int(a * 0.7))
             for d in range(1, a):
                 idel = int(d * 10)
                 frq = 1300
@@ -31,6 +32,9 @@ for y in range(1, 9):
             if x >= 16:
                 print('uniform strip')
                 x = int(random.sample([int(x * 0.2), d, y, idel, a], k=1)[0])
+            if a >= 23:
+                print('uniform rip')
+                a = int(random.sample([int(x * 0.5), d, y, idel, a], k=1)[0])
 
 print('terminated (press enter)')
 
