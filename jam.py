5ff7cda76f0860b463d4c5879d7faddb29f556d4
kilitary <kilitary@gmail.com>
2024-06-11 16:58:46 +0300

    insertio

--- jam.py
+++ jam.py
@@ -17,21 +17,26 @@ import random
 
 idel = 0
 d = 0
+xx = 0
 for y in range(1, 9):
     print(f'floOr {y}')
-    for x in range(random.randrange(6, 8), 35):
-        x += 3
-        for a in range(3, x - int(d * 0.4)):
+    for x in range(random.randrange(2, 32), 55):
+        x += 5 - int(y * 4.6)
+        xx = x
+        for a in range(3, x + int(d * 1.5)):
             a = random.randrange(1, int(a * 0.7))
             for d in range(1, a):
                 idel = int(d * 10)
                 frq = 1300
                 frq += int(d * idel)
-                print(f'{frq} {idel} {x} e{y} {a}')
+                print(f'frq={frq} idel={idel} x={x} e={y} a={a}')
                 winsound.Beep(frq, int(128 + idel + a + x))
-            if x >= 16:
+            if x == xx:
+                x = random.randrange(1, int(1 + a * 1.7))
+            xx = x
+            if x <= 20:
                 print('uniform strip')
-                x = int(random.sample([int(x * 0.2), d, y, idel, a], k=1)[0])
+                x = int(random.sample([int(x), d, y, idel, a], k=1)[0])
             if a >= 10:
                 print('uniform rip')
                 a = int(random.sample([int(x * 0.5), d, y, idel, a], k=2)[1])
