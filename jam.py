5288156202e408c42aa0372d2206ea4287303185
kilitary <kilitary@gmail.com>
2024-06-14 23:10:07 +0300

    insertio

--- jam.py
+++ jam.py
@@ -6,6 +6,7 @@
 # secret: se c ret
 # for: quiery
 # type: help
+# variant: 1-2-3-4
 
 # uninstruct io ns
 import winsound
@@ -40,8 +41,8 @@ def rail_1(a=0):
     vl = -vl * 0.1
     # vl = min(-30, min(10, int(vl)))
     if vl <= -30.0:
-        vl = -30.0
-    print(f'[{th_id:08x}-02] uniform trip vl: {vl:2.1f}  a: {a:-2d} sp: {sp:2.1f}')
+        vl = random.sample([-30.0, -20.0, -10.0], k=3)[0]
+    # print(f'\n[{th_id:08x}-02] uniform trip vl: {vl:2.1f}  a: {a:-2d} sp: {sp:2.1f}')
     volume.SetMasterVolumeLevel(vl, None)  # 10%
 
 
@@ -54,7 +55,7 @@ def rail_2(d=0, x=0, y=0, a=0, xx=0):
     frq_i %= 5134
     frq_i = max(37, frq_i)
 
-    rnl = random.randrange(min(x, a) + 1, max(d, xx) + 100)
+    rnl = random.randrange(min(x, a) + 1, min(x, a) + 1 + (max(d, xx) + 100))
     ln_i = min(260, 80 + (int(70 + idel + a + x + d) + rnl)) % 300
 
     return frq_i, ln_i
@@ -64,7 +65,7 @@ def rail_2(d=0, x=0, y=0, a=0, xx=0):
 
 # rail runner
 
-def rails_run(ii=0):
+def rails_run(ai=0):
     idel = 0
     d = random.sample([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], k=1)[0]
     for y in range(1, 9):
@@ -73,17 +74,16 @@ def rails_run(ii=0):
         for x in range(random.randrange(2, 32), 55):
 
             x += 5 - int(y * 1.6)
-
             xx = x
 
             for a in range(3, x + int(d * 1.5)):
-
-                idel = random.sample([y, x, a], k=1)[0]
-
+                idel = random.sample([y, x, a, 1111, 9999, 2222, 4444, 7777, 6666], k=1)[0]
                 a = random.randrange(1, int(a * 1.7))
+
                 if x == xx:
                     x = random.randrange(1, int(1 + a * 5.7))
                 xx = x
+
                 if x <= 20:
                     px = x
                     x = int(random.sample([int(x), d, y, idel, a], k=1)[0])
@@ -92,25 +92,31 @@ def rails_run(ii=0):
                     pa = a
                     a = int(random.sample([int(x * 0.5), d, y, idel, a], k=2)[1])
                     print(f'[{th_id:08x}-00] uniform rip {pa} => {a}')
+
                 prev_ln = 0
                 frq = 0
                 ln = 0
 
-                for d in range(1, a % 10):
+                print(f'\nret->{idel:04} ')
+
+                for d in range(1, a % 15):
 
                     frq, ln = rail_2(d)
                     if prev_ln > ln:
                         rail_1(a)
-                        print(f'\nret->{frq} ')
+                        print(f'>', end='')
+                    elif prev_ln < ln:
+                        print(f'<', end='')
+                    else:
+                        print(f'+', end='')
+
                     prev_ln = ln
 
-                    print(f'.', end='')
                     winsound.Beep(frq, ln)
 
                 print(f'\n\r')
                 print(f'[{th_id:08x}-xy] d={d} sp={sp} frq={frq} idel={idel} x={x} e={y} a={a} ln={ln}')
 
-                rail_1(a=a)
                 rail_2(a=a, x=x, y=y, d=d, xx=xx)
 
     return 0
