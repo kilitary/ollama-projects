fccf6228b346bf9e1a2745d67a70e6d046b49284
./deconf <deconf@ya.ru>
2024-06-24 07:48:13 +0300

    insertio

--- webui.cmd
+++ webui.cmd
@@ -1,2 +1,2 @@
-docker run -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+docker run -p 30000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 docker start open-webui
