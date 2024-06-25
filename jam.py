f361afc5a87d13ff6d6268597621ba9eb9d716f3
./deconf <deconf@ya.ru>
2024-06-25 19:55:28 +0300

    A little pain never hurt anyone.

--- jam.py
+++ jam.py
@@ -37,14 +37,17 @@ volume = cast(interface, POINTER(IAudioEndpointVolume))
 # + generalized
 
 # section global
-console = Console(width=122, style="white on black")
+console = Console(width=122)
 
-max_vol = 25
-min_vol = -30
-max_string_len = 18
-
+max_vol = 30.0
+min_vol = -35
+max_string_len = 11
+max_len = 455
+min_len = 200
 
 # 1nd rail - volume (implementation: speed)
+volume.SetMasterVolumeLevel(min_vol, None)
+
 
 # section volume
 def rail_vol(a=0):
@@ -60,7 +63,7 @@ def rail_vol(a=0):
         vl = random.randrange(min_vol, max_vol)
     else:
         vl = vlx
-    # print(f'\n[{th_id:08x}-02] uniform trip vl: {vl:2.1f}  a: {a:-2d} sp: {sp:2.1f} vlx: {vlx:2.1f}')
+    # console.print(f'\n[{th_id:08x}-02] uniform trip vl: {vl:2.1f}  a: {a:-2d} sp: {sp:2.1f} vlx: {vlx:2.1f}')
 
     try:
         volume.SetMasterVolumeLevel(vl, None)  # 10%
@@ -87,7 +90,7 @@ def rail_freq_len(d=0, x=0, y=0, a=0, xx=0):
         frq_i = max(37, frq_i)
 
     rnl = random.randrange(min(x, a) + 1, min(x, a) + 1 + (max(d, xx) + 100))
-    ln_i = min(260, 80 + (int(70 + idel + a + x + d) + rnl)) % 300
+    ln_i = max(min_len, 80 + (int(70 + idel + a + x + d) + rnl)) % max_len
 
     return frq_i, ln_i
 
@@ -101,40 +104,46 @@ def rails_run(ai=0):
     d = random.sample([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], k=10)[0]
     for y in range(1, 9):
         console.print(f'floOr {y}')
-
+        xx = 0
         for x in range(random.randrange(2, 32), 55):
-
             x += 5 - int(y * 1.6)
-            xx = x
 
             for a in range(3, x + int(d * 1.5)):
-                idel = random.sample([y, x, a, 1111, 9999, 2222, 4444, 7777, 6666], k=1)[0]
+                idel = random.sample([y, x, a, 111, 999, 222, 444, 777, 666], k=9)[0]
                 a = random.randrange(1, int(a * 1.7))
 
                 if x == xx:
-                    x = random.randrange(1, int(1 + a * 5.7))
+                    x = random.randrange(1, max(2, int(idel - a * 5.7)))
                 xx = x
 
                 if x <= 20:
                     px = x
-                    x = int(random.sample([int(x), d, y, idel, a], k=1)[0])
+                    x = int(random.sample([int(x), d, y, idel, a], k=5)[0])
                     # console.print(f'[{th_id:08x}-00] uniform strip {px} => {x}')
                 if a >= 10:
                     pa = a
-                    a = int(random.sample([int(x * 0.5), d, y, idel, a], k=2)[1])
+                    a = int(random.sample([int(x * 0.5), d, y, idel, a], k=5)[1])
                     # console.print(f'[{th_id:08x}-00] uniform rip {pa} => {a}')
 
                 prev_ln = 0
                 frq = 0
                 ln = 0
 
-                console.print(f'\nret->{idel:04} ')
+                console.print(f'\nret->{idel:03} ')
 
                 for d in range(1, a % max_string_len):
-
                     frq, ln = rail_freq_len(a=a, x=x, y=y, d=d, xx=xx)
 
-                    console.print(f'{frq:04d}:{ln:02d} ', end='')
+                    r = random.randrange(1, 3)
+
+                    if r == 2:
+                        st = 'cyan on white'
+                        rail_vol(a=a)
+                    else:
+                        st = 'green'
+
+                    console.print(f'{frq:04d}:{ln:03d}', end='', style=st)
+                    console.print(' ', end='')
 
                     prev_ln = ln
 
@@ -142,7 +151,7 @@ def rails_run(ai=0):
 
                 # console.print(f'[{th_id:08x}-xy] d={d} sp={sp} frq={frq} idel={idel} x={x} e={y} a={a} ln={ln}')
 
-                rail_freq_len(a=a, x=x, y=y, d=d, xx=xx)
+                rail_vol(a=a)
 
     return 0
 
