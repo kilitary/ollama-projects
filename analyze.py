bf04d41508a0d586d58cfda375ac1b1f6700c5d1
./deconf <deconf@ya.ru>
2024-07-03 19:52:18 +0300

    It is not that uncommon for the cost of an abstraction to outweigh the benefit it delivers. Kill one today! - John Carmack вот что он сказал ей

--- analyze.py
+++ analyze.py
@@ -98,21 +98,74 @@ for m in sorted_models:
             slog(f'[red]exception: {e}')
 
         options = {
+            # How long the model will stay loaded into memory.
+            #  The parameter (Default: 5 minutes) can be set to:
+            # 1. a duration string in Golang (such as "10m" or "24h");
+            # 2. a number in seconds (such as 3600);
+            # 3. any negative number which will keep the model loaded  in memory (e.g. -1 or "-1m");
+            # 4. 0 which will unload the model immediately after generating a response;
+            'keep_alive': 0,
+
+            # The temperature of the model. Increasing the temperature will make the model answer more creatively. (Default: 0.8)
             'temperature': temperature,
+
+            # The number of GPUs to use. On macOS it defaults to 1 to enable metal support, 0 to disable
+            'num_gpu': 1,
+
+            # Sets the size of the context window used to generate the next token. (Default: 2048)
             'num_ctx': num_ctx,
+
+            # use memory mapping
             'use_mmap': True,
+
+            # Sets the number of threads to use during computation.
+            # By default, Ollama will detect this for optimal performance.
+            # It is recommended to set this value to the number of physical
+            # CPU cores your system has (as opposed to the logical number of cores)
             'num_thread': 14,
+
+            # Force system to keep model in RAM
             'use_mlock': True,
-            'mirostat': 0,
+
+            # Enable Mirostat sampling for controlling perplexity. (default: 0, 0 = disabled, 1 = Mirostat, 2 = Mirostat 2.0)
+            # 'mirostat': 0,
+            # Sets how far back for the model to look back to prevent repetition. (Default: 64, 0 = disabled, -1 = num_ctx)
             'repeat_last_n': -1,
+
+            # Controls the balance between coherence and diversity of the output.
+            # A lower value will result in more focused and coherent text. (Default: 5.0)
             'mirostat_tau': 1.0,
+
+            # Sets the random number seed to use for generation.
+            # Setting this to a specific number will make the model generate the same text for the same prompt. (Default: 0)
             'seed': time.time_ns(),
-            'mirostat_eta': 0.1,
+
+            # Influences how quickly the algorithm responds to feedback from the generated text.
+            # A lower learning rate will result in slower adjustments, while a higher learning rate will make the algorithm more responsive.
+            # (Default: 0.1)
+            # 'mirostat_eta': 0.1,
+
+            # Tail free sampling is used to reduce the impact of less probable tokens from the output.
+            # A higher value (e.g., 2.0) will reduce the impact more, while a value of 1.0 disables this setting. (default: 1)
             # 'tfs_z': 1,
+
+            # Reduces the probability of generating nonsense.
+            # A higher value (e.g. 100) will give more diverse answers, while a lower value (e.g. 10) will be more conservative. (Default: 40)
             # 'top_k': 10,
+
+            # Works together with top-k. A higher value (e.g., 0.95) will lead to more diverse text,
+            # while a lower value (e.g., 0.5) will generate more focused and conservative text. (Default: 0.9)
             # 'top_p': 0.9,
+
+            # Maximum number of tokens to predict when generating text. (Default: 128, -1 = infinite generation, -2 = fill context)
             'num_predict': -2,
+
+            # Sets how strongly to penalize repetitions. A higher value (e.g., 1.5) will penalize repetitions more strongly,
+            # while a lower value (e.g., 0.9) will be more lenient. (Default: 1.1)
             # 'repeat_penalty': 1.51,
+
+            # Sets the stop sequences to use. When this pattern is encountered the LLM will stop generating text and return.
+            # Multiple stop patterns may be set by specifying multiple separate stop parameters in a modelfile.
             'stop': [
                 '<|user|>',
                 '<|assistant|>',
@@ -138,8 +191,6 @@ for m in sorted_models:
                 "</s>"
             ]
         }
-        # 'num_predict': 50000,
-        # Maximum number of tokens to predict when generating text. (Default: 128, -1 = infinite generation, -2 = fill context)
 
         # penalize_newline
         context = []
@@ -148,11 +199,12 @@ for m in sorted_models:
         slog(f'\n[blue]⁂ [yellow]{model}[/yellow] [red]thinking[/red] ... ', end='')
 
         templ = """
-        {{ if .System }}<|im_start|>system
-        {{ .System }}<|im_end|>
-        {{ end }}{{ if .Prompt }}<|im_start|>user
-        {{ .Prompt }}<|im_end|>
-        {{ end }}<|im_start|>assistant
+        {{ if.System}} < | im_start | > system
+        {{.System}} < | im_end | >
+                        {{end}}
+        {{ if.Prompt}} < | im_start | > user
+        {{.Prompt}} < | im_end | >
+                        {{end}} < | im_start | > assistant
         """
 
         for response in client.generate(
